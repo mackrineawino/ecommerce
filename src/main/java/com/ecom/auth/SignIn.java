b855d0a83546fc2516aed996cfffee287d6a8f15
Mackrine Awino <awinomackrine1@gmail.com>
2023-11-03 14:22:41 +0300

    implement login using the database contents

--- src/main/java/com/ecom/auth/SignIn.java
+++ src/main/java/com/ecom/auth/SignIn.java
@@ -1,9 +1,6 @@
 package com.ecom.auth;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,39 +9,47 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang3.StringUtils;
 
+import com.ecom.app.model.entity.User;
+import com.ecom.database.Database;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
 
 @WebServlet(urlPatterns = "/login")
 public class SignIn extends HttpServlet {
-    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
         HttpSession httpSession = req.getSession();
 
         if (StringUtils.isNotBlank((String) httpSession.getAttribute("loggedInId")))
-            res.sendRedirect("./home");
+            resp.sendRedirect("./home");
         else
-            res.sendRedirect("./");
+            resp.sendRedirect("./");
     }
 
-    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        HttpSession httpSession = req.getSession(true);
-        httpSession.setAttribute("loggedInId", new Date().getTime() + "");
-        
-        ServletContext ctx = getServletContext();
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
 
         String username = req.getParameter("username");
         String password = req.getParameter("password");
 
-        if (username.equals(ctx.getInitParameter("username"))
-        && password.equals(ctx.getInitParameter("password"))) {
-            ctx.setAttribute("username", username);
-            resp.sendRedirect("./home");
+        Database database = Database.getDbInstance();
 
-        } else {
-            PrintWriter print = resp.getWriter();
-            print.write("<html><body>Invalid login details <a href=\".\"> Login again </a></body></html>");
+
+        for (User user : database.getUsers()) {
+            if (username.equals(user.getUsername()) && password.equals(user.getPassword())) {
+                HttpSession httpSession = req.getSession(true);
+                httpSession.setAttribute("loggedInId", new Date().getTime() + "");
+                httpSession.setAttribute("username", username);
+
+                resp.sendRedirect("./home");
+
+            }
         }
 
+        PrintWriter print = resp.getWriter();
+        print.write("<html><body>Invalid login details <a href=\".\"> Login again </a></body></html>");
+
     }
+
 }
