d76e7cc5a0c7116a98abd792181f9528e5b0151c
Mackrine Awino <awinomackrine1@gmail.com>
2023-11-03 10:17:11 +0300

    add logout servlet

---
+++ src/main/java/com/ecom/auth/Logout.java
@@ -0,0 +1,18 @@
+package com.ecom.auth;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/logout")
+public class Logout  extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getSession().invalidate();
+        resp.sendRedirect("./");
+    }
+}
