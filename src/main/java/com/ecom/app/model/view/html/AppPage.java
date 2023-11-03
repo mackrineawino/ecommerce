466288654545f0dad934c2a52fae79ae95f1de24
Mackrine Awino <awinomackrine1@gmail.com>
2023-11-03 14:19:46 +0300

    uses sessions instead of context

--- src/main/java/com/ecom/app/model/view/html/AppPage.java
+++ src/main/java/com/ecom/app/model/view/html/AppPage.java
@@ -3,6 +3,7 @@ package com.ecom.app.model.view.html;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.ecom.app.model.view.css.AppCss;
 import com.ecom.app.model.view.navbar.NavBar;
@@ -13,45 +14,46 @@ import java.io.Serializable;
 
 public class AppPage implements Serializable {
 
-    public void renderHtml(HttpServletRequest req, HttpServletResponse resp,
-            int activeMenu, String content) throws IOException {
+        public void renderHtml(HttpServletRequest req, HttpServletResponse resp,
+                        int activeMenu, String content) throws IOException {
 
-        ServletContext ctx = req.getServletContext();
+                HttpSession session = req.getSession();
 
-        PrintWriter print = resp.getWriter();
+                ServletContext ctx = req.getServletContext();
 
-        print.write("<!DOCTYPE html>" +
-                "<html>" +
+                PrintWriter print = resp.getWriter();
 
-                "<head>" +
-                new AppCss().getStyle() +
-                "</head>" +
+                print.write("<!DOCTYPE html>" +
+                                "<html>" +
 
-                "<body>" +
-                "<div style=\" display: flex; justify-content: space-between;\">" +
-                "<div>" +
-                "<h2><a href=\"./home\" style=\"margin: 0px 20px; text-decoration: none; font-size: 30px; cursor: pointer;\"><span style=\"color: #E0588E;\"><span\n"
-                + //
-                "                        style=\"font-size: 50px\">C</span>ool</span></span><span style=\"color: #49A3C8;\"><span\n"
-                + //
-                "                    style=\"font-size: 50px\">S</span>tuff</span></a><h2> " +
-                "</div>" +
-                "<div>" +
-                new NavBar().menu(activeMenu) +
-                "</div>" +
-                "<div>" +
-                "<a href=\"./cart\"><img style=\" width: 25px; cursor: pointer;  margin: 40px 20px;\" +\" src=\"https://sun1-97.userapi.com/s/v1/if1/0noPLWMBCqPv3zilDYqZ8ZuBQ4vugB8GNMnTXPAl_FV6mpzTDECSCkNLZnep-Y0dU_1KYXp-.jpg?size=201x201&quality=96&crop=25,22,201,201&ava=1\"><a/>"
-                +
-                "</div>" +
-                "</div>" +
-                "<h3>" + ctx.getInitParameter("AppName") + "<h3>" +
-                "<br/>&nbsp;<br/>" +
-                "<h3>Welcome: " + ctx.getAttribute("username") + "</h3><br/>");
+                                "<head>" +
+                                new AppCss().getStyle() +
+                                "</head>" +
 
-        print.write(content);
-        print.write("<a href=\"./logout\">Logout</a>" +
-                "</body>" +
-                "</html>");
+                                "<body>" +
+                                "<div style=\" display: flex; justify-content: space-between;\">" +
+                                "<div>" +
+                                "<h2><a href=\"./home\" style=\"margin: 0px 20px; text-decoration: none; font-size: 30px; cursor: pointer;\"><span style=\"color: #E0588E;\"><span\n"
+                                + //
+                                "                        style=\"font-size: 50px\">C</span>ool</span></span><span style=\"color: #49A3C8;\"><span\n"
+                                + //
+                                "                    style=\"font-size: 50px\">S</span>tuff</span></a><h2> " +
+                                "</div>" +
+                                "<div>" +
+                                new NavBar().menu(activeMenu) +
+                                "</div>" +
+                                "<div>" +
+                                "<a href=\"./cart\"><img style=\" width: 25px; cursor: pointer;  margin: 40px 20px;\" +\" src=\"https://sun1-97.userapi.com/s/v1/if1/0noPLWMBCqPv3zilDYqZ8ZuBQ4vugB8GNMnTXPAl_FV6mpzTDECSCkNLZnep-Y0dU_1KYXp-.jpg?size=201x201&quality=96&crop=25,22,201,201&ava=1\"><a/>"
+                                +
+                                "</div>" +
+                                "</div>" +
+                                "<br/>&nbsp;<br/>" +
+                                "<h3>Welcome: " + session.getAttribute("username") + "</h3><br/>");
 
-    }
+                print.write(content);
+                print.write("<a href=\"./logout\">Logout</a>" +
+                                "</body>" +
+                                "</html>");
+
+        }
 }
