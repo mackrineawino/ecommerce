18020d9a8591e0b7f1db47ca0609d2f5643ee97d
Mackrine Awino <awinomackrine1@gmail.com>
2023-11-05 18:25:46 +0300

    changed signup usertype option to a radio button

--- src/main/java/com/ecom/app/model/view/html/AppPage.java
+++ src/main/java/com/ecom/app/model/view/html/AppPage.java
@@ -30,7 +30,7 @@ public class AppPage implements Serializable {
                                 ".navbar { display: flex; align-items: center; }" +
                                 ".navbar .logout-link { text-decoration: none; color: #49A3C8; font-size: 20px; cursor: pointer; margin-left: 20px; }"
                                 +
-                                "body { margin: 0; padding: 0px 50px; font-family: 'Fira Sans', sans-serif; background: white; }"+
+                                "body { margin: 0; padding: 0px 50px; font-family: 'Fira Sans', sans-serif; background: black; }"+
                                 "</style>" +
                                 "</head>" +
 
@@ -47,8 +47,8 @@ public class AppPage implements Serializable {
                                 + "<a href=\"./cart\"><img style=\"width: 50px; color: #49A3C8; cursor: pointer; margin: 40px 0px; margin-left: 20px;\" src=\"https://github.com/mackrineawino/images/blob/main/image-removebg-preview%20(3).png?raw=true\"></a>"
                                 + "</div>" +
                                 "</div>" +
-                                 "<h3 style=\" text-align: center; color: #49A3C8; font-size: 30px; margin: 10px 0;\">Hello " + session.getAttribute("username") + " welcome to CoolStuff</h3>" +
-                                 "<h3 style=\"text-align: center; margin-top: 20px; margin-left: 20px; font-size: 25px; color: #49A3C8;\">Shop Now</h3>");
+                                 "<h3 style=\" text-align: center; color: white; font-size: 30px; margin: 10px 0;\">Hello " + session.getAttribute("username") + "</h3>" +
+                                 "<h3 style=\"text-align: center; margin-top: 20px; margin-left: 20px; font-size: 25px; color: white;\">Shop Now</h3>");
 
                 print.write(content);
                 print.write(
