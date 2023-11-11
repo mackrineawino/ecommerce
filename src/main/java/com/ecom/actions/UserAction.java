f5c8c3e0bbfae9a860f0d266e0f0351de666e3bd
Mackrine Awino <awinomackrine1@gmail.com>
2023-11-12 01:16:31 +0300

    implemented jsp for rendering admin page

--- src/main/java/com/ecom/actions/UserAction.java
+++ src/main/java/com/ecom/actions/UserAction.java
@@ -1,6 +1,8 @@
 package com.ecom.actions;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,14 +10,14 @@ import javax.servlet.http.HttpServletResponse;
 import com.ecom.app.bean.UserBeanI;
 import com.ecom.app.bean.UserBeanImpl;
 import com.ecom.app.model.entity.User;
-import com.ecom.app.model.view.html.SignUpPage;
 
 @WebServlet("/user")
 public class UserAction extends BaseAction {
     UserBeanI userBean = new UserBeanImpl();
 
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        new SignUpPage().renderSignup(req, resp);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("./register.jsp");
+        dispatcher.forward(req, resp);
     }
 
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
